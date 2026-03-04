c91757dee890209301e352fd38ddd65f4c814bc1
La-Kiks <kilian.audroin@gmail.com>
2026-03-04 11:28:55 +0100

    Refactor Footer and Navbar styles for improved responsiveness; enhance NotFound page layout and styles; adjust body height and root styles for better structure

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import "./NotFound.scss";
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <Link to="/">Go back Home</Link>
+    <div className="notfound">
+      <h1 className="notfound__title">404</h1>
+      <h2 className="notfound__subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/" className="notfound__link">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
